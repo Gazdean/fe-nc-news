7098ab2946c39beb833ca26080bcbea9457945f7
Gareth Dean <133035724+Gazdean@users.noreply.github.com>
2024-02-06 10:36:28 +0000

    Revert "5 view individual article"

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,19 +1,16 @@
-import { Link } from "react-router-dom";
+export default function ArticleCard({article}) {
 
-export default function ArticleCard({ article }) {
   return (
     <>
       <li className="article_card">
         <h3 id="article_title">{article.title}</h3>
 
         <div className="article_details">
-          <Link to={`/article/${article.article_id}`}>
-            <img
-              className="article_image"
-              src={article.article_img_url}
-              alt={`representing the topic of ${article.topic}`}
-            />
-          </Link>
+          <img
+            className="article_image"
+            src={article.article_img_url}
+            alt={`representing the topic of ${article.topic}`}
+          />
           <div className="list_details">
             <p>Author: {article.author}</p>
             <p>Topic: {article.topic}</p>
