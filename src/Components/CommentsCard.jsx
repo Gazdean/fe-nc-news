7182253a4d76d0afb9be19240bafbe4e1e1ab566
Gareth Dean <gazdean@hotmail.com>
2024-02-07 23:34:24 +0000

    ticket 9 delete a comment

--- src/Components/CommentsCard.jsx
+++ src/Components/CommentsCard.jsx
@@ -1,8 +1,25 @@
 import { useContext } from "react";
 import { SignedInContext } from "../contexts/SignedinContext";
+import { useState } from "react";
+import { removeCommentByCommentId } from "../utils/api-utils";
 
 export default function CommentsCard({ comment }) {
   const { signedIn, setSignedIn } = useContext(SignedInContext);
+  const [isDeleting, setIsDeleting] = useState(false)
+
+  function handleDeleteOnclick(){
+   
+    setIsDeleting(true)
+    removeCommentByCommentId(comment.comment_id)
+    .then((response)=> {
+      if(response.status === 204) {
+        setIsDeleting(false)
+      }
+    })
+    .catch((err)=> {
+      console.log(err)
+    })
+  }
 
   return (
     <>
@@ -11,7 +28,7 @@ export default function CommentsCard({ comment }) {
         <p className="comment_details">Comment by {comment.author}</p>
         <p className="comment_details">Votes: {comment.votes}</p>
       </li>
-      {comment.author === signedIn.username ? <button>delete</button>: null}
+      {comment.author !== signedIn.username ?  null : isDeleting ? <p>Deleting your message</p> : <button onClick= {handleDeleteOnclick}>delete</button> }
     </>
   );
 }
