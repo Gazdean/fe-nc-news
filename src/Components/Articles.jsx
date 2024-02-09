251965d0fd444e3c336bfedb7322c40a975ba91a
Gareth Dean <gazdean@hotmail.com>
2024-02-09 14:45:02 +0000

    css on drop down menus

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -10,26 +10,28 @@ export default function Articles() {
   const [allArticles, setAllArticles] = useState([]);
   const [topics, setTopics] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [filter, setFilter] = useState("false")
+  const [filter, setFilter] = useState("false");
   const [searchParams, setSearchParams] = useSearchParams("");
 
   const topicSearchParam = searchParams.get("topic");
-  const sortBySearchParam = searchParams.get("sort_by");
+  let sortBySearchParam = searchParams.get("sort_by");
   const orderSearchParam = searchParams.get("order");
 
   useEffect(() => {
-    
-    const paramObject ={}
+    const paramObject = {};
 
-    if(topicSearchParam) {
-      paramObject.topic = topicSearchParam}
-    if(sortBySearchParam) {
-      paramObject.sort_by = sortBySearchParam}
-    if(orderSearchParam) {
-      paramObject.order = orderSearchParam}
+    if (topicSearchParam) {
+      paramObject.topic = topicSearchParam;
+    }
+    if (sortBySearchParam) {
+      if (sortBySearchParam === "date") sortBySearchParam = "created_at";
+      paramObject.sort_by = sortBySearchParam;
+    }
+    if (orderSearchParam) {
+      paramObject.order = orderSearchParam;
+    }
 
     fetchAllArticles(paramObject)
-
       .then((receivedArticles) => {
         setAllArticles(receivedArticles);
         setIsLoading(false);
@@ -50,31 +52,31 @@ export default function Articles() {
   }, []);
 
   function handleFilter() {
-    if (filter===false) setFilter(true)
-    else setFilter(false)
+    if (filter === false) setFilter(true);
+    else setFilter(false);
   }
 
   function handleSelectTopic(event) {
-    const topic = event.target.value
+    const topic = event.target.value;
     const newParams = new URLSearchParams(searchParams);
     newParams.set("topic", topic);
-    setSearchParams(newParams)
+    setSearchParams(newParams);
   }
 
   function handleSelectSortBy(event) {
-    const sortBy = event.target.value
+    const sortBy = event.target.value;
     const newParams = new URLSearchParams(searchParams);
     newParams.set("sort_by", sortBy);
-    setSearchParams(newParams) 
+    setSearchParams(newParams);
   }
 
   function handleSelectOrder(event) {
-    const order = event.target.value
+    const order = event.target.value;
     const newParams = new URLSearchParams(searchParams);
     newParams.set("order", order);
     setSearchParams(newParams);
   }
-  
+
   return (
     <div>
       <ShareButtons />
@@ -82,30 +84,46 @@ export default function Articles() {
         <p>...your articles are loading</p>
       ) : (
         <>
-          <label htmlFor="topics">Pick a topic: </label>
-          <select onChange={handleSelectTopic} id="topics" >
-            <option value="">All topics</option>
-            {topics.map((topic) => {
-              return <TopicOptions key={topic.slug} topic={topic} />;
-            })}
-          </select>
-             <button onClick={handleFilter}>{filter ? "Close Filter" : "Open filter"}</button>
-        {filter ?
-            <>
-          <label htmlFor="sort-by">Sort by: {}</label>
-          <select onChange={handleSelectSortBy} id="sort-by" >
-            <option value="created_at">date</option>
-            <option value="comment_count">Comment count</option>
-            <option value="votes">Votes</option>
-          </select>
+          <div id="topic-container">
+            <label htmlFor="topics">Pick a topic: </label>
+            <select
+             
+              onChange={handleSelectTopic}
+              id="topics"
+            >
+              <option  className="drop-down" value="">All topics</option>
+              {topics.map((topic) => {
+                return <TopicOptions key={topic.slug} topic={topic} />;
+              })}
+            </select>
+            <button id="filter-button" onClick={handleFilter}>
+              {filter ? "Close Filter" : "Open filter"}
+            </button>
+          </div>
+          {filter ? (
+            <div id="filter-container">
+              <label htmlFor="sort-by">Sort by: {}</label>
+              <select
+                className="drop-down"
+                onChange={handleSelectSortBy}
+                id="sort-by"
+              >
+                <option value="date">date</option>
+                <option value="comment_count">Comment count</option>
+                <option value="votes">Votes</option>
+              </select>
 
-          <label htmlFor="order">order: {}</label>
-          <select onChange={handleSelectOrder} id="order" >
-            <option value="desc">Descending</option>
-            <option value="asc">Ascending</option>
-          </select>
-          </> : null
-}
+              <label htmlFor="order">order: {}</label>
+              <select
+                className="drop-down"
+                onChange={handleSelectOrder}
+                id="order"
+              >
+                <option className="option" value="desc">Descending</option>
+                <option className="option" value="asc">Ascending</option>
+              </select>
+            </div>
+          ) : null}
           <ul className="article_container">
             {allArticles.map((article) => {
               return <ArticleCard key={article.article_id} article={article} />;
